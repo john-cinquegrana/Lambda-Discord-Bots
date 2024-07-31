dacf846b675056f1e8c58d279e75dd157043b8e6
Jazz <alllegron@gmail.com>
2024-07-31 00:42:26 +0000

    Included tutorial code for Discord Bot

--- minecraft-discord-bot/hello_world/app.py
+++ minecraft-discord-bot/hello_world/app.py
@@ -1,6 +1,8 @@
 import json
 
-# import requests
+# Import crypto packages for discord Auth
+from nacl.signing import VerifyKey
+from nacl.exceptions import BadSignatureError
 
 
 def lambda_handler(event, context):
@@ -33,10 +35,63 @@ def lambda_handler(event, context):
 
     #     raise e
 
+    try:
+        body = json.loads(event['body'])
+            
+        signature = event['headers']['x-signature-ed25519']
+        timestamp = event['headers']['x-signature-timestamp']
+
+        # validate the interaction
+
+        verify_key = VerifyKey(bytes.fromhex(PUBLIC_KEY))
+
+        message = timestamp + event['body']
+        
+        try:
+            verify_key.verify(message.encode(), signature=bytes.fromhex(signature))
+        except BadSignatureError:
+            return {
+                'statusCode': 401,
+                'body': json.dumps('invalid request signature')
+            }
+        
+        # handle the interaction
+
+        t = body['type']
+
+        if t == 1:
+            return {
+                'statusCode': 200,
+                'body': json.dumps({
+                'type': 1
+                })
+            }
+        elif t == 2:
+            return command_handler(body)
+        else:
+            return {
+                'statusCode': 400,
+                'body': json.dumps('unhandled request type')
+            }
+    except:
+        raise
+
+def command_handler(body):
+  command = body['data']['name']
+
+  if command == 'hello':
     return {
-        "statusCode": 200,
-        "body": json.dumps({
-            "message": "hello world",
-            # "location": ip.text.replace("\n", "")
-        }),
+      'statusCode': 200,
+      'headers' : {'Content-Type': 'application/json'},
+      'body': json.dumps({
+        'type': 4,
+        'data': {
+          'content': 'Hello, World.',
+        }
+      })
+    }
+  else:
+    return {
+      'statusCode': 400,
+      'body': json.dumps('unhandled command')
     }
